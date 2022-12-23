f08e7a5d699aa6f2425be6ae9b954c02c9d3b3e4
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2022-12-23 12:02:39 -0500

    Add basic decoding by type

--- lib/github/client.ex
+++ lib/github/client.ex
@@ -7,7 +7,8 @@ defmodule GitHub.Client do
     [
       {JasonSerializer, :encode_body},
       {HTTPoisonClient, :request},
-      {JasonSerializer, :decode_body}
+      {JasonSerializer, :decode_body},
+      {__MODULE__, :decode_as_type}
     ]
   end
 
@@ -21,4 +22,37 @@ defmodule GitHub.Client do
       end
     end)
   end
+
+  def decode_as_type(operation) do
+    %Operation{response_body: body, response_code: code, response_types: types} = operation
+
+    if type = get_type(types, code) do
+      decoded_body = do_decode(body, type)
+      {:ok, %Operation{operation | response_body: decoded_body}}
+    else
+      {:ok, operation}
+    end
+  end
+
+  defp get_type(types, code) do
+    if res = Enum.find(types, fn {c, _} -> c == code end) do
+      elem(res, 1)
+    end
+  end
+
+  defp do_decode(%{} = value, {module, type}) do
+    fields = module.__fields__(type)
+
+    for {field_name, field_type} <- fields, reduce: struct(module) do
+      decoded_value ->
+        if field_value = Map.get(value, to_string(field_name)) do
+          decoded_field_value = do_decode(field_value, field_type)
+          struct(decoded_value, [{field_name, decoded_field_value}])
+        else
+          decoded_value
+        end
+    end
+  end
+
+  defp do_decode(value, _type), do: value
 end
