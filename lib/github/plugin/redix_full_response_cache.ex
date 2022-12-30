7fd3a39cbe103d9c28c30b02bb1540e138927662
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2022-12-30 14:29:38 -0500

    Begin docs and config for redix cache

--- lib/github/plugin/redix_full_response_cache.ex
+++ lib/github/plugin/redix_full_response_cache.ex
@@ -1,7 +1,62 @@
 if Code.ensure_loaded?(Redix) do
   defmodule GitHub.Plugin.RedixFullResponseCache do
+    @moduledoc """
+    Use Redis to cache full API responses and perform conditional requests
+
+    > ### Warning {:.warning}
+    >
+    > This plugin caches full API responses. This may include sensitive data, and it may use a lot
+    > of memory. Memory usage can be controlled using the `expiration` configuration.
+
+    GitHub's API uses [Conditional Requests](https://docs.github.com/en/rest/overview/resources-in-the-rest-api#conditional-requests)
+    to provide fast responses when the client already has the latest information. Furthermore, apps
+    with heavy API usage benefit from the fact that 304 responses don't count against rate limits.
+
+    This module provides two plugins: `check_cache/2` and `use_cache/2`. When run before an API
+    request, `check_cache/2` will check the configured Redis server for a cached response matching
+    the request's URL and auth information. If found, the cached response's ETag will be added to
+    the request headers as an `If-None-Match` header.
+
+    If the API returns a `304 Not Modified` response, then `use_cache/2` will fill in the cached
+    response body and reset the status to `200`. Otherwise, if the API returns a `200` response with
+    new data, that response will be placed in the cache.
+
+    ## Configuration
+
+      * `server`: **Required** global name of a Redis server, as found in the `name` option of
+        `Redix.start_link/2`. This library is not responsible for starting the Redix app or
+        connecting to the server.
+
+      * `cache_prefix`: Prefix to use for all Redis cache keys. Defaults to `"oapi_github"`.
+
+      * `expiration`: Time, in seconds, before cached data should expire. Defaults to 30 days.
+
+    These options may be passed via the plugin definition or by global configuration:
+
+        config :oapi_github,
+          stack: [
+            {#{inspect(__MODULE__)}, :check_cache, server: :redix_server_1},
+            # ...
+          ]
+
+        config :oapi_github, #{inspect(__MODULE__)},
+          server: :redix_server_1
+
+    ## Cache Keys
+
+    Cache keys used by this module have the following parts (separated by colons):
+
+      * A standard prefix (see `prefix` configuration),
+      * The server for the request (ex. `api.github.com`),
+      * The path of the request (ex. `/repos/aj-foster`),
+      * URL-encoded parameters (ex. `page=1`), and
+      * A SHA-256 hash of the auth token used.
+
+    By including the hashed auth token in the key, we can be reasonably sure that...
+    """
     alias GitHub.Operation
 
+    @default_cache_prefix "oapi_github"
     @default_expiration_sec 60 * 60 * 24 * 30
     @private_key :redix_full_response_cache
 
@@ -66,8 +121,13 @@ if Code.ensure_loaded?(Redix) do
             response_code: 304,
             response_headers: headers
           } = operation,
-          _opts
+          opts
         ) do
+      server = config!(opts, :server)
+      key = cache_key(operation)
+      expiration = config(opts, :expiration, @default_expiration_sec)
+      Redix.noreply_command(server, ["EXPIRE", key, expiration, "GT"])
+
       operation = %Operation{
         operation
         | response_body: response,
@@ -87,7 +147,8 @@ if Code.ensure_loaded?(Redix) do
     @spec cache_key(Operation.t()) :: String.t()
     defp cache_key(operation) do
       [
-        "oapi_github",
+        cache_key_prefix(),
+        cache_key_server(operation),
         cache_key_url(operation),
         cache_key_params(operation),
         cache_key_auth(operation)
@@ -112,6 +173,14 @@ if Code.ensure_loaded?(Redix) do
       |> Enum.sort_by(fn {key, _value} -> key end)
       |> URI.encode_query()
     end
+
+    @spec cache_key_prefix :: String.t()
+    defp cache_key_prefix do
+      config([], :cache_prefix, @default_cache_prefix)
+    end
+
+    @spec cache_key_server(Operation.t()) :: String.t()
+    defp cache_key_server(%Operation{request_server: server}), do: server
 
     @spec cache_key_url(Operation.t()) :: String.t()
     defp cache_key_url(%Operation{request_url: url}), do: url
