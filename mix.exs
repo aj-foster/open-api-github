517f46c56672589c472efdb1ad647548ce375737
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2023-02-18 00:35:21 -0500

    Allow HTTPoison 2.0

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule GitHub.MixProject do
     [
       {:ex_doc, "~> 0.29", only: :dev, runtime: false},
       {:jason, "~> 1.0", optional: true},
-      {:httpoison, "~> 1.7", optional: true},
+      {:httpoison, "~> 1.7 or ~> 2.0", optional: true},
       {:oapi_generator, "0.0.2", only: [:dev, :test], runtime: false},
       {:redix, "~> 1.0", optional: true}
     ]
