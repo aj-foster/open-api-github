95309f79b950b9254e151f625b182ba74bd86434
AJ Foster <2789166+aj-foster@users.noreply.github.com>
2023-09-26 21:33:15 -0400

    Avoid compiling processor in test env

--- lib/github/processor.ex
+++ lib/github/processor.ex
@@ -1,9 +1,11 @@
-defmodule GitHub.Processor do
-  @moduledoc false
-  use OpenAPI.Processor
+if Mix.env() == :dev do
+  defmodule GitHub.Processor do
+    @moduledoc false
+    use OpenAPI.Processor
 
-  def operation_docstring(state, operation_spec, params) do
-    OpenAPI.Processor.Operation.docstring(state, operation_spec, params)
-    |> String.replace("](/", "](https://docs.github.com/")
+    def operation_docstring(state, operation_spec, params) do
+      OpenAPI.Processor.Operation.docstring(state, operation_spec, params)
+      |> String.replace("](/", "](https://docs.github.com/")
+    end
   end
 end
